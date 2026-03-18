88ff658a84837b2878934f7446a52632652faff1
Laurents <andikagendel11@gmail.com>
2026-03-18 11:56:37 +0700

    Update index.js with new features and status
    Removed previous version and routing configuration, added new features and status.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 {
-  "version": 2,
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/index.json"
-    }
-  ]
+  "verAddr": "https://ffaja.vercel.app/",
+  "resetGuest": true,
+  "status": "VIP_PREMIUM_ACTIVE",
+  "features": {
+    "unlock_all_skins": true,
+    "unlock_all_emotes": true,
+    "unlock_all_weapons": true,
+    "bypass_security": "enabled_v5",
+    "visual_skin_enable": 1
+  },
+  "msg": "LRN v1.1.1 - FIX 404 UNTUK BOCAH TOLOL"
 }
